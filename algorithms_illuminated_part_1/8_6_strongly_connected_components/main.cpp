6d165301ada47ebf708c8113e9d078ead832030e
Фоменко Степан Владимирович <stepannk@mail.ru>
2021-03-02 16:37:50 +0300

    Replace recursive dfs with stack-based

--- algorithms_illuminated_part_1/8_6_strongly_connected_components/main.cpp
+++ algorithms_illuminated_part_1/8_6_strongly_connected_components/main.cpp
@@ -6,6 +6,8 @@
 #include <vector>
 #include <cassert>
 #include <algorithm>
+#include <cstring>
+#include <stack>
 
 void topology_order_internal(const std::vector<std::vector<size_t>>& adj_list,
 							 std::vector<bool>& is_visited,
@@ -25,12 +27,36 @@ void topology_order_internal(const std::vector<std::vector<size_t>>& adj_list,
 
 std::vector<size_t> topology_order(const std::vector<std::vector<size_t>>& adj_list) {
 	std::vector<size_t> ordered_vertexes(adj_list.size(), 0);
-	std::vector<bool> is_visited(adj_list.size(), false);
+	std::vector<char> is_visited(adj_list.size(), 0);
 	size_t order_counter = adj_list.size();
 
-	for (size_t v = 0; v < adj_list.size(); v++) {
-		if (is_visited[v]) continue;
-		topology_order_internal(adj_list, is_visited, ordered_vertexes, order_counter, v);
+	std::stack<size_t> stack;
+	std::stack<size_t> stack_v_idx;
+	for (size_t i = 0; i < adj_list.size(); i++) {
+		if (is_visited[i]) continue;
+		stack.push(i);
+		stack_v_idx.push(0);
+		is_visited[i] = true;
+		while (!stack.empty()) {
+			auto v = stack.top();
+
+			if (stack_v_idx.top() >= adj_list[v].size()) {
+				stack_v_idx.pop();
+				stack.pop();
+
+				order_counter--;
+				ordered_vertexes[order_counter] = v;
+
+			} else {
+				auto w = adj_list[v][stack_v_idx.top()];
+				stack_v_idx.top()++;
+
+				if (is_visited[w]) continue;
+				is_visited[w] = true;
+				stack.push(w);
+				stack_v_idx.push(0);
+			}
+		}
 	}
 
 	return ordered_vertexes;
@@ -40,11 +66,19 @@ void dfs_scc(const std::vector<std::vector<size_t>>& adj_list,
 			 std::vector<bool>& is_visited,
 			 std::vector<size_t>& scc_vertexes,
 			 size_t s) {
+
+	std::stack<size_t> stack;
+	stack.push(s);
 	is_visited[s] = true;
-	scc_vertexes.push_back(s);
-	for (auto v: adj_list[s]) {
-		if (is_visited[v]) continue;
-		dfs_scc(adj_list, is_visited, scc_vertexes, v);
+	while (!stack.empty()) {
+		auto v = stack.top();
+		stack.pop();
+		scc_vertexes.emplace_back(v);
+		for (auto w: adj_list[v]) {
+			if (is_visited[w]) continue;
+			is_visited[w] = true;
+			stack.push(w);
+		}
 	}
 }
 
@@ -56,7 +90,7 @@ void dfs_scc(const std::vector<std::vector<size_t>>& adj_list,
 std::vector<std::vector<size_t>> scc(const std::vector<std::vector<size_t>>& adj_list) {
 	auto ordered_vertexes = topology_order(adj_list);
 
-	std::vector<bool> is_visited(adj_list.size(), false);
+	std::vector<bool> is_visited(adj_list.size(), 0);
 	std::vector<std::vector<size_t>> components;
 
 	for (auto v_it = ordered_vertexes.rbegin(); v_it < ordered_vertexes.rend(); v_it++) {
