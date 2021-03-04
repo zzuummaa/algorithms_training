019934e62ac5478ff25b759e8b510bc772df8877
Stepan Fomenko <stepannk@mail.ru>
2021-03-04 10:59:18 +0300

    Fix Kosaraju algorithm using true transposed graph

--- algorithms_illuminated_part_2/8_6_strongly_connected_components/main.cpp
+++ algorithms_illuminated_part_2/8_6_strongly_connected_components/main.cpp
@@ -7,22 +7,6 @@
 #include <cstring>
 #include <stack>
 
-void topology_order_internal(const std::vector<std::vector<size_t>>& adj_list,
-							 std::vector<bool>& is_visited,
-							 std::vector<size_t>& ordered_vertexes,
-							 size_t& order_counter,
-							 size_t v) {
-	is_visited[v] = true;
-
-	for (auto w: adj_list[v]) {
-		if (is_visited[w]) continue;
-		topology_order_internal(adj_list, is_visited, ordered_vertexes, order_counter, w);
-	}
-
-	order_counter--;
-	ordered_vertexes[order_counter] = v;
-}
-
 std::vector<size_t> topology_order(const std::vector<std::vector<size_t>>& adj_list) {
 	std::vector<size_t> ordered_vertexes(adj_list.size(), 0);
 	std::vector<char> is_visited(adj_list.size(), 0);
@@ -80,18 +64,28 @@ void dfs_scc(const std::vector<std::vector<size_t>>& adj_list,
 	}
 }
 
+std::vector<std::vector<size_t>> transposed_graph(const std::vector<std::vector<size_t>>& adj_list) {
+	std::vector<std::vector<size_t>> adj_list_transposed(adj_list.size());
+	for (size_t v = 0; v < adj_list.size(); v++) {
+		for (auto w: adj_list[v]) {
+			adj_list_transposed[w].emplace_back(v);
+		}
+	}
+	return adj_list_transposed;
+}
+
 /**
  * Algorithm Kosaraju
  *
  * @return strongly connected components with vertexes
  */
 std::vector<std::vector<size_t>> scc(const std::vector<std::vector<size_t>>& adj_list) {
-	auto ordered_vertexes = topology_order(adj_list);
+	auto ordered_vertexes = topology_order(transposed_graph(adj_list));
 
-	std::vector<bool> is_visited(adj_list.size(), 0);
+	std::vector<bool> is_visited(adj_list.size(), false);
 	std::vector<std::vector<size_t>> components;
 
-	for (auto v_it = ordered_vertexes.rbegin(); v_it < ordered_vertexes.rend(); v_it++) {
+	for (auto v_it = ordered_vertexes.begin(); v_it < ordered_vertexes.end(); v_it++) {
 		auto v = *v_it;
 		if (is_visited[v]) continue;
 		components.emplace_back();
@@ -130,6 +124,19 @@ int main() {
 	assert((components[1] == std::vector<size_t>{ 2, 4, 7, 9 }));
 	assert((components[2] == std::vector<size_t>{ 6, 8, 10   }));
 	assert((components[3] == std::vector<size_t>{ 11         }));
+
+	std::vector<std::vector<size_t>> b = {
+			{ 1, 2 },
+			{ 0    },
+			{      }
+	};
+	components = scc(b);
+	for (auto& component: components) {
+		std::sort(component.begin(), component.end());
+	}
+	std::sort(components.begin(), components.end(), [](auto& a, auto& b){ return a.front() < b.front(); });
+	assert((components[0] == std::vector<size_t>{ 0, 1 }));
+	assert((components[1] == std::vector<size_t>{ 2    }));
 
 	for (const auto & in_file_name : testcase_iterator("./")) {
 		std::cout << in_file_name << std::endl;
